fdd63ac1484f7181f0b2ab1587a1f78dfddfc279
Luiz Vicente <luizcv89@gmail.com>
2023-01-22 20:26:56 -0300

    Deplyed to sepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+const {config} = require("dotenv");
+const {resolve} = require("path");
+
+const dotenvConfigPath = process.env.DOTENV_CONFIG_PATH || "./.env";
+config({ path: resolve(__dirname, dotenvConfigPath) });
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
   },
 };
